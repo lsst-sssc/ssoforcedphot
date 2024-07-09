eca5854a2b74b34971f4ccf60246ded4b1b5829b
szilac <laszlo.szigeti.86@gmail.com>
2024-07-09 14:16:59 +0200

    modified:   src/forcedphot/__init__.py

--- src/forcedphot/__init__.py
+++ src/forcedphot/__init__.py
@@ -1,3 +1,5 @@
 from .example_module import greetings, meaning
+from .horizons_interface import HorizonsInterface
+from .local_dataclasses import QueryInput, QueryResult, EphemerisData
 
-__all__ = ["greetings", "meaning"]
+__all__ = ["greetings", "meaning", "HorizonsInterface", "QueryInput", "QueryResult", "EphemerisData"]
