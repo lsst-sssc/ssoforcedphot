7356fc97b66277b40528f9cf8e0f16eb5bcb5121
szilac <laszlo.szigeti.86@gmail.com>
2024-07-19 20:51:18 +0200

    modified:   src/forcedphot/__init__.py 	modified:   src/forcedphot/ephemeris/__init__.py

--- src/forcedphot/__init__.py
+++
@@ -1,3 +0,0 @@
-# forcedphot/__init__.py
-
-from . import ephemeris
--- src/forcedphot/ephemeris/__init__.py
+++
@@ -1,4 +0,0 @@
-# forcedphot/ephemeris/__init__.py
-
-from .horizons_interface import HorizonsInterface
-from .local_dataclasses import QueryInput, EphemerisData, QueryResult
