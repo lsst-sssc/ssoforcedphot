2beb9ba511f70fbb10b10cb5f47a935695fb07fd
Laszlo Szigeti <laszlo.szigeti.86@gmail.com>
2025-03-13 11:37:56 +0000

    modified:   tests/forcedphot/conftest.py

---
+++ tests/forcedphot/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+def pytest_ignore_collect(path, config):
+    """Skip collecting test modules that require LSST."""
+    if path.basename in ['test_image_service.py', 'test_photometry_service.py', 'test_odc.py']:
+        return True
+    return False
