38d6a06074ff7af8a460b587f00cde2b6116d936
Laszlo Szigeti <laszlo.szigeti.86@gmail.com>
2025-03-13 11:20:23 +0000

    new file:   tests/conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+try:
+    import forcedphot
+except ImportError:
+    pass
+
+try:
+    import lsst.geom
+    import lsst.afw
+except ImportError:
+    def pytest_collection_modifyitems(config, items):
+        print("Skipping LSST-dependent tests...")
+        skip_lsst = pytest.mark.skip(reason="LSST modules not available")
+        for item in items:
+            item.add_marker(skip_lsst)
